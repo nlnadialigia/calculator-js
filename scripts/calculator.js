ac796ea227fb68939c41407e49d2084ec6e22465
Nadia Ligia <nlnadialigia@gmail.com>
2021-02-16 13:08:08 -0300

    feat: create class calcController with her getters and setters

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -2,5 +2,4 @@
 /* eslint-disable new-parens */
 /* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
-/* eslint-disable eol-last */
 let calculator = new CalcController;
--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,4 +1,24 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-expressions */
 class CalcController {
+  constructor () {
+    this._displayCalc = '0';
+    this._dataAtual;
+  }
 
+  get displaCalc () {
+    return this._displayCalc;
+  }
+
+  set displaCalc (value) {
+    this.displaCalc = value;
+  }
+
+  get dataAtual () {
+    return this._dataAtual;
+  }
+
+  set dataAtual (value) {
+    this.dataAtual = value;
+  }
 }
