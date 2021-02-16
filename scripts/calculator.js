c23b6b7634c9a8d14ba0ac0b97a254066bb7919a
Nadia Ligia <nlnadialigia@gmail.com>
2021-02-16 12:44:43 -0300

    feat: add folder controller

---
+++ scripts/calculator.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+/* eslint-disable new-parens */
+/* eslint-disable prefer-const */
+/* eslint-disable no-unused-vars */
+/* eslint-disable eol-last */
+let calculator = new CalcController;
