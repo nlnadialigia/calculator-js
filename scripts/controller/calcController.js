e9878277ac54234595b4ba42594410046d997f1b
Nadia <nlnadialigia@gmail.com>
2021-02-18 06:30:34 -0300

    feat: add initial config at calculator display

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -3,7 +3,18 @@
 class CalcController {
   constructor () {
     this._displayCalc = '0';
-    this._dataAtual;
+    this._currentDate;
+    this.initialize();
+  }
+
+  initialize () {
+    const displayCalcEl = document.querySelector('#display');
+    const dateEl = document.querySelector('#data');
+    const timeEl = document.querySelector('#hora');
+
+    displayCalcEl.innerHTML = '0';
+    dateEl.innerHTML = '01/05/2020';
+    timeEl.innerHTML = '00.00';
   }
 
   get displaCalc () {
@@ -14,11 +25,11 @@ class CalcController {
     this.displaCalc = value;
   }
 
-  get dataAtual () {
-    return this._dataAtual;
+  get currentDate () {
+    return this._currentDate;
   }
 
-  set dataAtual (value) {
-    this.dataAtual = value;
+  set currentDate (value) {
+    this.currentDate = value;
   }
 }
