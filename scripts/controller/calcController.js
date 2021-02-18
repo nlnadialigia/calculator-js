f1cec2cdd8e9273b4f6963788788b2f11a474bc9
Nadia Ligia <nlnadialigia@gmail.com>
2021-02-18 13:22:29 -0300

    feat: add custom addEventListener

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -20,12 +20,22 @@ class CalcController {
     }, 1000);
   }
 
+  addEventListenerAll(element, events, fn){
+    events.split(' ').forEach(event => {
+      element.addEventListener(event, fn, false);
+    });
+  }
+
   initButtonsEvents(){
     const buttons = document.querySelectorAll('#buttons > g, #parts > g');
 
     buttons.forEach((btn, index) => {
-      btn.addEventListener('click', e => {
+      this.addEventListenerAll(btn, 'click drag', e => {
         console.log(btn.className.baseVal.replace('btn-', ''));
+      });
+
+      this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
+        btn.style.cursor = 'pointer';
       });
     });
   }
