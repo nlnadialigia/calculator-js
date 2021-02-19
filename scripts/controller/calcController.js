230b0a32c52b442da3e76c477d7d4a8fdfc2c2ba
Nadia Ligia <nlnadialigia@gmail.com>
2021-02-19 08:23:16 -0300

    feat: add Switch to controll all calculator buttons

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-expressions */
 class CalcController {
   constructor(){
+    this._operation = [];
     this._locale = 'pt-BR';
     this._displayCalcEl = document.querySelector('#display');
     this._dateEl = document.querySelector('#data');
@@ -26,12 +27,79 @@ class CalcController {
     });
   }
 
+  clearAll(){
+    this._operation = [];
+  }
+
+  clearEntry(){
+    this._operation.pop();
+  }
+
+  addOperation(value){
+    this._operation.push(value);
+    console.log(this._operation);
+  }
+
+  setError(){
+    this.displayCalc = 'Error';
+  }
+
+  execBtn(value){
+    switch (value) {
+      case 'ac':
+        this.clearAll();
+        break;
+      case 'ce':
+        this.clearEntry();
+        break;
+      case 'soma':
+
+        break;
+      case 'subtracao':
+
+        break;
+      case 'multiplicacaoo':
+
+        break;
+      case 'divisao':
+
+        break;
+      case 'porcento':
+
+        break;
+      case 'igual':
+
+        break;
+      case 'ponto':
+
+        break;
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        this.addOperation(parseInt(value));
+        break;
+      default:
+        this.setError();
+        break;
+    }
+  }
+
   initButtonsEvents(){
     const buttons = document.querySelectorAll('#buttons > g, #parts > g');
 
     buttons.forEach((btn, index) => {
       this.addEventListenerAll(btn, 'click drag', e => {
         console.log(btn.className.baseVal.replace('btn-', ''));
+        const textBtn = btn.className.baseVal.replace('btn-', '');
+
+        this.execBtn(textBtn);
       });
 
       this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
